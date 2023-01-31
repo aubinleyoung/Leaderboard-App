59d4d2c67b65e29076771b01c455bc4560240ecf
aubin sinpeze <aubinsimpeze01@gmail.com>
2023-01-31 17:16:52 +0200

    Add array to store datas in the index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import _ from 'lodash';
 import './style.css';
- 
+ const listBlock=document.querySelector('.srores-list-container');
+const scores =[
+{
+  name:'Aubin',
+  score:85,
+},
+{
+  name:'Mthieux',
+  score:75,
+},
+{
+  name:'Marc',
+  score:65,
+},
+{
+  name:'Luc',
+  score:55,
+}
+];
+scores.map((e)=>{
+  listBlock.innerHTML+=` 
+  <div class="scores-list">${e.name} : ${e.score}</div>
+  `
+})
