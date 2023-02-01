d4592a84013b7896cb8aecdb6c030518a7a362f0
aubin sinpeze <aubinsimpeze01@gmail.com>
2023-02-01 22:11:31 +0200

    Add function to implement the submit and refresh buttons

--- src/index.js
+++ src/index.js
@@ -1,28 +1,56 @@
 // import _ from 'lodash';
 import './style.css';
 
-const listBlock = document.querySelector('.srores-list-container');
-const scores = [
-  {
-    name: 'Aubin',
-    score: 85,
-  },
-  {
-    name: 'Mthieux',
-    score: 75,
-  },
-  {
-    name: 'Marc',
-    score: 65,
-  },
-  {
-    name: 'Luc',
-    score: 55,
-  },
-];
-scores.map((e) => {
-  listBlock.innerHTML += ` 
-  <div class="scores-list">${e.name} : ${e.score}</div>
-  `;
-  return listBlock;
+const refreshScores = async (gameId) => {
+  try {
+    const response = await fetch(`https://api.leaderboard.com/games/${gameId}/scores`);
+    const scores = await response.json();
+
+    const scoresList = document.getElementById('scoresList');
+    scoresList.innerHTML = ''; // Clear the previous scores
+
+    scores.forEach((score) => {
+      const scoreItem = document.createElement('li');
+      scoreItem.innerHTML = `${score.player}: ${score.score}`;
+      scoresList.appendChild(scoreItem);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Function to submit a score for a game
+const submitScore = async (gameId, playerName, score) => {
+  try {
+    const data = { player: playerName, score };
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    };
+    const response = await fetch(`https://api.leaderboard.com/games/${gameId}/scores`, options);
+    const result = await response.json();
+    console.log(result);
+
+    refreshScores(gameId); // Refresh the scores after submitting a new score
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+const gameId = 'abc123';
+
+// Refresh button click event handler
+document.getElementById('refreshButton').addEventListener('click', () => {
+  refreshScores(gameId);
+ 
+});
+
+// Submit button click event handler
+document.getElementById('submitButton').addEventListener('click', () => {
+  const playerName = document.getElementById('playerName').value;
+  const score = document.getElementById('score').value;
+  submitScore(gameId, playerName, score);
+ 
 });
